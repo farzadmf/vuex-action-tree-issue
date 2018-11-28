a329c77c1de53cbd7da2c50d320c4c5102c66614
Farzad Majidfayyaz <fmajidfayyaz@genetec.com>
2018-11-28 10:59:48 -0500

    Add mock tests!

--- src/actions.spec.ts
+++ src/actions.spec.ts
@@ -1,10 +1,12 @@
 import { actions as a } from './constants';
 import { typelessActions, typeSafeActions } from './actions';
 
-const testActions = () => {
-  typeSafeActions.hello();                        // ERROR: Type 'Action<EndUserState, {}>' has no compatible call signatures
+describe('Actions', () => {
+  it('should not have an error here!!!', () => {
+    typeSafeActions.hello();                        // ERROR: Type 'Action<EndUserState, {}>' has no compatible call signatures
 
-  const commit = () => {};                        // Or: commit = jest.fn()
-  typelessActions.hello();                        // OK! But it doesn't have access to 'commit'
-  typelessActions[a.CREATE_USER]({ commit }, {}); // NOT GOOD! I have to pass 'commit' manually
-};
+    const commit = jest.fn();
+    typelessActions.hello();                        // OK! But it doesn't have access to 'commit'
+    typelessActions[a.CREATE_USER]({ commit }, {}); // NOT GOOD! I have to pass 'commit' manually
+  })
+});
